aec0a6ed0db3fcb2ccc46fee7b296c25e0e6367c
Rodrigo Alencar Bernardo <rodrigo@kayala.com.br>
2015-08-26 09:03:43 -0300

    add --force-yes to apt-get install

--- vagrant/vagrant_setup_root.sh
+++ vagrant/vagrant_setup_root.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 #cleanup unused packages
-apt-get -y remove puppet puppet-common chef
+apt-get -y --force-yes remove puppet puppet-common chef
 
 # set locale
 locale-gen pt_BR.UTF-8
 
 # git
-apt-get -y install git
+apt-get -y --force-yes install git
 
 # install nodejs 0.12
 curl -sL https://deb.nodesource.com/setup_0.12 | bash -
-apt-get -y install nodejs
-apt-get -y install build-essential
-apt-get -y install libfontconfig
+apt-get -y --force-yes install nodejs
+apt-get -y --force-yes install build-essential
+apt-get -y --force-yes install libfontconfig
+apt-get -y --force-yes install g++
 
 npm -g upgrade npm
 npm install -g grunt-cli bower yo generator-karma generator-angular --silent
